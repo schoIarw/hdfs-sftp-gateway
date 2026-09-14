db20254126ecf04cfb1cb9d26e179f9e1b9a6e4f
schoIarw <schoIarw@users.noreply.github.com>
2026-09-14 08:21:46 +0000

    feat: Run This Project
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- hfg-manager-api/src/main/java/io/github/scholiarw/hfg/manager/api/HfgManagerApplication.java
+++ hfg-manager-api/src/main/java/io/github/scholiarw/hfg/manager/api/HfgManagerApplication.java
@@ -2,9 +2,13 @@ package io.github.scholiarw.hfg.manager.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "io.github.scholiarw.hfg")
+@EntityScan(basePackages = "io.github.scholiarw.hfg")
+@EnableJpaRepositories(basePackages = "io.github.scholiarw.hfg.manager.infra")
 @EnableScheduling
 public class HfgManagerApplication {
   public static void main(String[] args) {
