a9df7bd31264ddeb036fcb0b5cd5b75da1d75437
samuel <scholarw@hotmail.com>
2026-09-16 14:31:26 +0800

    test: provide audit dependency in auth slice

--- hfg-manager-api/src/test/java/io/github/scholiarw/hfg/manager/api/AuthControllerTest.java
+++ hfg-manager-api/src/test/java/io/github/scholiarw/hfg/manager/api/AuthControllerTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AuthController.class)
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
     })
 class AuthControllerTest {
   @Autowired private MockMvc mockMvc;
+  @MockitoBean private JdbcClient jdbcClient;
 
   @Test
   void returnsTheAuthenticatedAdministrator() throws Exception {
